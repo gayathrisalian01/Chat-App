aa7d1a49fe1b14afb1ae22cb6124df0f55c7a07b
Gayathri <gayathripsalian2001@gmail.com>
2024-02-21 10:09:01 +0530

    Initial Commit

---
+++ client/src/App.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import "./App.css";
+import Chat from "./Chat"
+import io from "socket.io-client";
+
+const socket=io.connect("http://localhost:3002")
+function App(){
+  const[username,setUsername]=useState("");
+  const[room,setRoom]=useState("");
+  const[showChat,setShowChat]=useState(false);
+  const joinRoom=()=>{
+    if(username!==""&&room!==""){
+      socket.emit("join_room",room)
+      setShowChat(true);
+    }
+  }
+  return (
+
+    <div className="App">
+    {!showChat ?(
+    <div className="joinChatContainer">
+    <h3>Join a chat</h3>
+    <input type="text" id="username" autoComplete='true' placeholder="Username" onChange={(event)=>{setUsername(event.target.value)}}/>
+    <input type="text" id="room" autoComplete='true' placeholder="Room Id"onChange={(event)=>{setRoom(event.target.value)}}/>
+    <button onClick={joinRoom}>Join a room</button>
+     </div>)
+     :
+     (
+     <Chat socket={socket} username={username} room={room} />
+  )}
+    </div>
+  );
+}
+ export default App;
